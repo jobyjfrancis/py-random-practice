102998f3278093eef635bf8c05d0348af2707fd8
Joby Francis <joby.francis@silverstripe.com>
2024-12-11 15:25:16 +1300

    more exercises

---
+++ remove_characters.py
@@ -0,0 +1,7 @@
+def remove_chars(s, n):
+    print(s[n:])
+
+your_str = input("Enter a string: ")
+remove_characters_number = int(input("How many characters would you like to remove: "))
+
+remove_chars(your_str, remove_characters_number)
--- string_index.py
+++ string_index.py
@@ -1,9 +1,19 @@
-str_input = input("Enter a string: ")
+"""str_input = input("Enter a string: ")
 print("The string you entered: ", str_input)
 print("Here are the characters at even index of this string: ")
 
 for i in range(0, len(str_input), 2):
-    print(str_input[i])
+    print(str_input[i])"""
+
+# Another method using list slicing
+str_input = input("Enter a string: ")
+print("The string you entered: ", str_input)
+print("Here are the characters at even index of this string: ")
+
+str_list = list(str_input)
+for i in str_list[0::2]:
+    print(i)
+
 
 
 
